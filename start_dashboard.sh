6645c4766688a1c0753163fb3a7efdb290ad1cd2
Phil White <phil@philwhite.org>
2020-09-15 06:18:54 -0700

    start_dashboard.sh: updates for autologin

--- start_dashboard.sh
+++ start_dashboard.sh
@@ -6,10 +6,10 @@ xset s off
 xset s noblank
 xset -dpms
 
-# Allow quitting the X server with CTRL-ATL-Backspace
+# Allow quitting the X server with CTRL-ALT-Backspace
 setxkbmap -option terminate:ctrl_alt_bksp
 
 # Start Chromium in kiosk mode
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --disable-infobars --check-for-update-interval=31536000 --kiosk /home/whitepa/plumbing-dashboard/index.html
+chromium-browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --kiosk /usr/local/plumbing-dashboard/index.html
